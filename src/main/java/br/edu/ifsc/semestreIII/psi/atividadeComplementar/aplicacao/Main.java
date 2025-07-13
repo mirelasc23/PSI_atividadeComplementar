2f6b74528bfd09185b667a53cd15a3d67036bdc4
mirelasc23 <mirelamsccomach@gmail.com>
2025-07-13 15:35:17 -0300

    adaptacao branch renata

--- src/main/java/br/edu/ifsc/semestreIII/psi/atividadeComplementar/aplicacao/Main.java
+++ src/main/java/br/edu/ifsc/semestreIII/psi/atividadeComplementar/aplicacao/Main.java
@@ -1,21 +1,29 @@
 package br.edu.ifsc.semestreIII.psi.atividadeComplementar.aplicacao;
 
+import br.edu.ifsc.semestreIII.psi.atividadeComplementar.entidades.*;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
+        //var scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Digite a matricula do Aluno");
-        var aluno = new Aluno(scanner.next());
+        ///var aluno = new Aluno(scanner.next());
+        Aluno aluno = new Aluno(scanner.next());
 
-        var modalidades = Repositorio.INSTANCE.getModalidades();
-        var menuModalidades = new StringBuilder("==Menu de Modalidades==\n");                //adiciona item ao menu
+        //var modalidades = Repositorio.INSTANCE.getModalidades();
+        //var menuModalidades = new StringBuilder("==Menu de Modalidades==\n");                //adiciona item ao menu
+        List<Modalidade> modalidades = Repositorio.INSTANCE.getModalidades();
+        StringBuilder menuModalidades = new StringBuilder("==Menu de Modalidades==\n");                //adiciona item ao menu
         for (int i = 0; i < modalidades.size(); i++) {                                       // monta o menu
             menuModalidades.append(i + 1 + " - " + modalidades.get(i).getNome() + "\n");     //index + 1 resulta no numero correspondente ao menu para o usuario escolher
         }
         menuModalidades.append("0 - Finalizar e emitir parecer.\nEscolha uma das opções");
 
-        var opcao = 0;
+        //var opcao = 0;
+        int opcao = 0;
+        
         do {
             System.out.println(menuModalidades);
             opcao = scanner.nextInt() - 1;
@@ -27,14 +35,7 @@ public class Main {
             }
         } while (opcao >= 0);
 
-        //TODO aqui iniciar o processo de validacoes das atividades declaradas que esta no Repositorio
-
-
-
-
-
-
-
+        
 
     }
 }
