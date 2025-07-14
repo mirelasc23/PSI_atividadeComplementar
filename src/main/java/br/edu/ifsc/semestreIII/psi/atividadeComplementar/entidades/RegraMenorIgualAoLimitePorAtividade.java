d1fa3f1a2fb328d118439e63750ae36215d67306
mirelasc23 <mirelamsccomach@gmail.com>
2025-07-14 11:30:03 -0300

    import regras

---
+++ src/main/java/br/edu/ifsc/semestreIII/psi/atividadeComplementar/entidades/RegraMenorIgualAoLimitePorAtividade.java
@@ -0,0 +1,18 @@
+package br.edu.ifsc.semestreIII.psi.atividadeComplementar.entidades;
+
+public class RegraMenorIgualAoLimitePorAtividade implements Regra{
+	private double somaHoras;
+	private Requerimento requerimento;
+    
+    public RegraMenorIgualAoLimitePorAtividade(double somaHoras, Requerimento requerimento) {
+        this.somaHoras = somaHoras;
+		this.requerimento = requerimento;
+    }
+    
+    @Override
+    public void isSatisfiedBy() {
+        if(this.somaHoras < requerimento.horasComplementares()){
+            System.out.println("Requerimento indeferido. \nMotivo: não atingiu as horas mínimas necessárias.");
+        } 
+    }  
+}
